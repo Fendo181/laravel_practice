27fbeb3b02c71d2e1499ee3915eff62a4ae72eac
Fendo181 <fedno181git@gmail.com>
2018-10-04 23:46:53 +0900

    p49 会員登録時にmailtrapにメールを送信するようにした

--- myApp/app/Listeners/RegisteredListener.php
+++ myApp/app/Listeners/RegisteredListener.php
@@ -2,20 +2,25 @@
 
 namespace App\Listeners;
 
+use App\User;
 use Illuminate\Auth\Events\Registered;
+use Illuminate\Mail\Mailer;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Contracts\Queue\ShouldQueue;
 
 class RegisteredListener
 {
+    private $mailer;
+    private $eloquent;
     /**
      * Create the event listener.
      *
      * @return void
      */
-    public function __construct()
+    public function __construct(Mailer $mailer, User $user)
     {
-        //
+        $this->mailer = $mailer;
+        $this->eloquent = $user;
     }
 
     /**
@@ -26,6 +31,9 @@ class RegisteredListener
      */
     public function handle(Registered $event)
     {
-        //
+        $user = $this->eloquent->findOrFail($event->user->getAuthIdentifier());
+        $this->mailer->raw('会員登録が完了しました！',function ($message) use ($user){
+            $message->subject('会員登録メール')->to($user->email);
+        });
     }
 }
