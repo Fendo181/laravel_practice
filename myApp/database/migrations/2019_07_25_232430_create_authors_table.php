d6fe560f0ee2863a73febb694ee7547d2e5cbbae
Fendo181 <fendo181git@gmail.com>
2019-07-26 08:40:03 +0900

    authorsテーブルのkanaカラムはnullを許可するようにする

--- myApp/database/migrations/2019_07_25_232430_create_authors_table.php
+++ myApp/database/migrations/2019_07_25_232430_create_authors_table.php
@@ -16,7 +16,7 @@ class CreateAuthorsTable extends Migration
         Schema::create('authors', function (Blueprint $table) {
             $table->bigIncrements('id');
             $table->string('name','100');
-            $table->string('kana','100');
+            $table->string('kana','100')->nullable();
             $table->timestamps();
         });
     }
