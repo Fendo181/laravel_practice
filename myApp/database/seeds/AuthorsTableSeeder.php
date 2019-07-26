f279255f73d5fa9e868d23592947237c4c0a3bc6
Fendo181 <fendo181git@gmail.com>
2019-07-26 09:08:59 +0900

    Seederを使ってauthorsテーブルに10件、サンプルデータが入るようにした

--- myApp/database/seeds/AuthorsTableSeeder.php
+++ myApp/database/seeds/AuthorsTableSeeder.php
@@ -11,6 +11,16 @@ class AuthorsTableSeeder extends Seeder
      */
     public function run()
     {
-        //
+        // Authorsテーブルに10件登録を行う
+        $now = \Carbon\Carbon::now();
+        for($i = 1; $i <= 10; $i++ ){
+            $author = [
+                'name' => 'authors'. $i,
+                'kana' => 'kana'. $i,
+                'created_at' => $now,
+                'updated_at' => $now,
+            ];
+            DB::table('authors')->insert($author);
+        }
     }
 }
