de131864aad388c91d09b503ff895806bfc0226c
Fendo181 <fendo181git@gmail.com>
2019-08-06 09:15:16 +0900

    Bookdetailモデルを使って、テストデータを入れる為のFactoryを用意する

--- myApp/database/factories/ModelFactory.php
+++ myApp/database/factories/ModelFactory.php
@@ -5,8 +5,15 @@
 use App\Model;
 use Faker\Generator as Faker;
 
-$factory->define(Model::class, function (Faker $faker) {
+$factory->define(App\Bookdetail::class, function (Faker $faker) {
+    $faker->locale('ja_jp');
+    $now = \Carbon\Carbon::now();
+
     return [
-        //
+        'isbn' => $faker->isbn13,
+        'published_date' => $faker->date($format = 'Y-m-d', $max = 'now'),
+        'price' => $faker->randomNumber(4),
+        'created_at' => $now,
+        'updated_at' => $now,
     ];
 });
