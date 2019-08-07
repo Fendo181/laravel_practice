b43785c5e28767adcc86c673f218d98dac02a598
Fendo181 <fendo181git@gmail.com>
2019-08-07 09:05:13 +0900

    $fillableでホワイトリスト方式を定義する

--- myApp/app/Author.php
+++ myApp/app/Author.php
@@ -9,4 +9,13 @@ class Author extends Model
     protected $table = 'authors';
     // 任意のカラムをprimary keyにする事ができる
     protected $primaryKey = 'id';
+
+    // ホワイトリスト方式
+    protected $fillable = [
+        'name',
+        'kana'
+    ];
+
+//    $fillableと$guardedは同時には使えない
+//    protected $guarded;
 }
